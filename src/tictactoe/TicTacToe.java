b8e0c1d064ab9cfea85e3a1bc02bbe7b86f263a2
Moustafa Essam <drsh_201122@yahoo.com>
2022-12-31 17:58:19 +0200

    add landing

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,33 +1,58 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
  */
 package tictactoe;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import tictactoe.base.TicTacToeHandle;
+import tictactoe.landing.LandingViewController;
+import tictactoe.resources.ResourcesLoader;
+import tictactoe.resources.styles.Styles;
+import tictactoe.router.StackRouter;
+import tictactoe.router.Router;
 
-/**
- *
- * @author Karim
- */
-public class TicTacToe extends Application {
+public class TicTacToe extends Application implements TicTacToeHandle {
+
+    private Router router;
+    private final ResourcesLoader resourcesLoader = new ResourcesLoader() {
+    };
+    
+    @Override
+    public Router router() {
+        return router;
+    }
+
+    @Override
+    public ResourcesLoader resourcesLoader() {
+        return resourcesLoader;
+    }
+
+    @Override
+    public void setupScene(Scene scene) {
+        scene.getStylesheets().add(resourcesLoader.getCss(Styles.BASE_STYLE_STRING).toString());
+        scene.getStylesheets().add(resourcesLoader.getCss(Styles.TABVIEW_STYLE_STRING).toString());
+    }
     
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(new Label("Hello"));
-        stage.setScene(scene);
+        setupStage(stage);
+        router = new StackRouter(this, stage);
+        router.push(new LandingViewController());
         stage.show();
     }
-
+    
+    private void setupStage(Stage stage) {
+        stage.setMinHeight(720);
+        stage.setMinWidth(1024);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
