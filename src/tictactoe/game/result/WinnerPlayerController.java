2494fd9a53c4362e12691efbd4ab0a8cfb98cbf1
Mariam-Adly <mariamadly712@gmail.com>
2023-01-11 22:35:32 +0200

    add alert

--- src/tictactoe/game/result/WinnerPlayerController.java
+++ src/tictactoe/game/result/WinnerPlayerController.java
@@ -47,8 +47,8 @@ public class WinnerPlayerController extends RouteViewController{
     public void initialize(URL url, ResourceBundle rb) {
         scene().getStylesheets().add(resourcesLoader().getCss(Styles.BASE_STYLE_STRING).toString());
         background.setEffect(UIHelper.createBlurEffect());
-         file = new File("");
-         media = new Media(file.toURI().toString());
+        file = new File("");
+        media = new Media(file.toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.play();
         videoView.setMediaPlayer(mediaPlayer);
